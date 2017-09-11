76f673ccdf15dde5ab9f72651044f4c2f9104227
vagrant <kiel.strang@gmail.com>
2017-09-11 23:32:25 +0000

    Add error handling

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const request = require('request');
 const fs = require('fs');
 
@@ -17,9 +16,9 @@ function downloadImageByURL(url, filepath) {
     .pipe(fs.createWriteStream(filepath));
 }
 
-function downloadContributorAvatar(contributor) {
-  const localPath = `avatars/${contributor.login}.jpg`;
-  downloadImageByURL(contributor.avatar_url, localPath);
+function downloadContributorAvatar(contributor, path) {
+  const fullPath = `${path}/${contributor.login}.jpg`;
+  downloadImageByURL(contributor.avatar_url, fullPath);
 }
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -39,22 +38,49 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+function downloadRepoContributorAvatars() {
+  console.log('Welcome to the GitHub Avatar Downloader');
 
+  //read repo owner and name from the command line - fail if not given 2 arguments
+  const args = require('optimist').argv;
+  if (args._.length !== 2) {
+    console.log('Please specify the repo owner and name');
+    console.log(args);
+    return;
+  }
+  const repoOwner = args._[0];
+  const repoName = args._[1];
 
-console.log('Welcome to the GitHub Avatar Downloader!');
-const repoOwner = process.argv[2];
-const repoName = process.argv[3];
+  //read a GitHub access token from the .env file - fail if not found
+  require('dotenv').config();
+  if(typeof process.env.GITHUB_TOKEN !== 'string') {
+    console.log('Please add a GitHub access token to the .env file (GITHUB_TOKEN=<your_token>)');
+    return;
+  }
 
-if(repoOwner && repoName) {
-  getRepoContributors(repoOwner, repoName, (err, contributors) => {
+  getRepoContributors(repoOwner, repoName, (err, response) => {
+    //handle errors returned from GitHub
     if (err) { throw err; }
+    if (response.message === 'Not Found') {
+      console.log('No repo found with the specified name and owner');
+      return;
+    }
+    if (response.message === 'Bad credentials') {
+      console.log('Invalid GitHub access token');
+      return;
+    }
 
-    for (const contributor of contributors) {
-      downloadContributorAvatar(contributor);
+    const path = 'avatars';
+    if(!fs.existsSync(path)) {
+      console.log(`Missing folder: ./${path}`);
+      return;
+    }
+
+    for (const contributor of response) {
+      downloadContributorAvatar(contributor, path);
     }
     console.log('Avatars downloaded!');
   });
-} else {
-  console.log('Please specify the repo owner and name!');
 }
 
+downloadRepoContributorAvatars();
