2b8c7c0ff78b1f33a60106c841f83eee921a1991
vagrant <kiel.strang@gmail.com>
2017-09-11 21:07:51 +0000

    Get list of contributors

--- download_avatars.js
+++ download_avatars.js
@@ -6,8 +6,19 @@ const GITHUB_USER = "kielstrang";
 const GITHUB_TOKEN = "bc5c433206b4bfac90f93add3e77277cda758b15";
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  const requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors'`;
+  const requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
   console.log(requestURL);
+
+  const options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+
+  request(options, (err, response, body) => {
+    console.log(body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
