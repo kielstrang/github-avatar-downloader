086c4e74d196ade35188f50735098f8d6f715a2d
vagrant <kiel.strang@gmail.com>
2017-09-11 21:01:46 +0000

    Build request URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,16 @@
-var request = require('request');
+const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+const GITHUB_USER = "kielstrang";
+const GITHUB_TOKEN = "bc5c433206b4bfac90f93add3e77277cda758b15";
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  const requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors'`;
+  console.log(requestURL);
 }
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
